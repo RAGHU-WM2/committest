8cdc46f99960356105245b4bc042a47db1068615
RAGHU-WM2 <raghuselvam@wmtwo.com>
2024-02-10 20:21:07 +0530

    just  rafc created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar'
 function App() {
   return (
     <div className="App">
+    <Navbar/>
    <h1>Test Commit  update 1</h1>
     </div>
   );
