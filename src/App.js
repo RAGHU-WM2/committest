c6b308180f76a2c5832ce9633eb17242935fa0eb
RAGHU-WM2 <raghuselvam@wmtwo.com>
2024-02-22 18:14:55 +0530

    commits4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar'
 function App() {
   return (
     <div className="App">
    <h1>Test Commit  update 1</h1>
+ <p>none demo</p>
+ <h1>end</h1>
     </div>
   );
 }
